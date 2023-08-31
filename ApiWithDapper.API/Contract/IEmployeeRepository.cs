44271a824dc567358af2cbb1b7b38e81e8325f95
Muhydeen Adebayo <adediranma@examroom.ai>
2023-08-31 17:06:14 +0100

    Employee Repo Started

---
+++ ApiWithDapper.API/Contract/IEmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using ApiWithDapper.API.Entities;
+
+namespace ApiWithDapper.API.Contract
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> GetEmployeesAsync();
+        Task<Employee> GetEmployeeByIdAsync(int id);
+        Task<string> RemoveEmployeebyId(int id);
+        Task<string> UpdateEmployeeInformation(int Id, EmployeeDto dto);
+        Task<Employee> AddEmployee(EmployeeDto dto);
+        Task<IEnumerable<Employee>> GetEmployeesForACompany(int companyId);
+    }
+}
---
+++ ApiWithDapper.API/Entities/EmployeeDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiWithDapper.API.Entities
+{
+    public class EmployeeDto
+    {
+        public int CompanyId { get; set; }
+        public string? Name { get; set; }
+        public string? Position { get; set; }
+        public int Age { get; set; }
+    }
+}
